c2ff8fb7a4c515407ce6868558b9a808630fec7c
Bruno Araujo <bruno.cadoma@gmail.com>
2024-02-18 21:01:45 -0300

    Alterando estrutura de dados do baralho

--- src/baralho/Baralho.java
+++ src/baralho/Baralho.java
@@ -1,6 +1,8 @@
 package baralho;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,15 +13,17 @@ public class Baralho {
         cartas = new ArrayList<>();
         // Preencher o baralho com cartas
         String[] naipes = {"Ouros", "Espadas", "Copas", "Paus"};
-        ValorCarta[] valores = {ValorCarta.QUATRO, ValorCarta.CINCO, ValorCarta.SEIS, ValorCarta.SETE,
-                ValorCarta.Q, ValorCarta.J, ValorCarta.K, ValorCarta.A,
-                ValorCarta.DOIS, ValorCarta.TRES};
+        Set<String> cartasAdicionadas = new HashSet<>(); // Criar combinações únicas de valor e naipe
         for (String naipe : naipes) {
-            for (ValorCarta valor : valores) {
-                cartas.add(new CartaNormal(naipe, valor.name(), valor));
+            for (ValorCarta valor : ValorCarta.values()) { // Iterar sobre todos os valores do enum
+                String chave = valor.name() + " de " + naipe; // Combinação de valor e naipe
+                if (cartasAdicionadas.add(chave)) { // Verificar se a combinação ainda não foi adicionada
+                    cartas.add(new CartaNormal(naipe, valor.name(), valor));
+                }
             }
         }
     }
+
 
 
     public void embaralhar() {
